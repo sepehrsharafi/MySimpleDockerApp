0431db68ac5b69c3f8857e843c9cc1a22bd6f7b8
sepehrsharafi <sepehr.sharafi123@gmail.com>
2024-08-21 20:44:49 +0330

    Added Workflow

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 const app = express();
-const port = 3000;
+
+const port = 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
