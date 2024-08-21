8099c85aa1a8c8f4b59496c4635ff33dcf8a8b35
sepehrsharafi <sepehr.sharafi123@gmail.com>
2024-08-21 19:12:31 +0330

    Bug fix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log(`Listening on port ${port}`);
 });
